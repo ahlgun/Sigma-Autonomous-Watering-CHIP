65876bb1591a2e349dcba738e7b82642e8b2ce5e
nauberinho <nauber91@hotmail.com>
2017-10-24 13:59:09 +0200

    server rigged

--- serverside/server.js
+++ serverside/server.js
@@ -3,3 +3,14 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    io.emit('saySomething', 'Hello World');
+    socket.on('saySomething', function(msg){
+        database.ref('communication/' + 'chip').set(msg);
+    });
+})
+
+server.listen(3000, () => {
+    console.log("Nu lyssnar vi på 3000.");
+})
