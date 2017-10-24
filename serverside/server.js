2ab11cdc2323a33300486325f0039db388b26e86
nauberinho <nauber91@hotmail.com>
2017-10-24 15:12:33 +0200

    connecting to endpoint

--- serverside/server.js
+++ serverside/server.js
@@ -4,12 +4,20 @@ var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function(socket) {
-    io.emit('saySomething', 'Hello World');
-    socket.on('saySomething', function(msg){
-        console.log(msg)
-    });
-})
+(function(){
+    let Req = new XMLHttpRequest();
+    Req.open('POST', 'http://localhost:7000/api/cars/filter',  false);
+    Req.setRequestHeader("Content-Type", "application/json");
+    Req.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+
+            response = JSON.parse(Req.response);
+            console.log(response)
+
+        }
+    };
+
+})();
 
 server.listen(3000, () => {
     console.log("Nu lyssnar vi på 3000.");
